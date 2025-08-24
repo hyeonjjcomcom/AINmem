7b546bb19188d7705cbee9ab8dcecbc50c4f4080
hyeonjjcomcom <hyeonjj@comcom.ai>
2025-08-24 21:53:25 +0900

    Chores: add d3 code and remove tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
-// app/layout.tsx
 import './globals.css';
+import { Metadata, Viewport } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Knowledge Graph',
+  description: 'Visualizing relationships between logical propositions',
+};
+
+// 뷰포트 설정을 별도로 분리
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+};
 
 export default function RootLayout({
   children,
