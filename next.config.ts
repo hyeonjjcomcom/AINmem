090b006bec952e78086a16a04f431fa577a88722
ainize dev <ainize-dev@comcom.ai>
2025-09-23 08:40:13 +0000

    next.config.ts edited online with Bitbucket

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: [
-      '@ainblockchain/ain-util',
-      '@ainblockchain/ain-js',
-      'bip39',
-      'eccrypto',
-      'secp256k1',
-      'bn.js',
-      'rlp',
-      'varuint-bitcoin'
-    ]
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push({
-        'eccrypto': 'commonjs eccrypto',
-        'secp256k1': 'commonjs secp256k1',
-        'bip39': 'commonjs bip39',
-        'bn.js': 'commonjs bn.js',
-        'rlp': 'commonjs rlp',
-        'varuint-bitcoin': 'commonjs varuint-bitcoin'
-      });
-    }
-    return config;
-  }
+  /* config options here */
 };
 
 export default nextConfig;
